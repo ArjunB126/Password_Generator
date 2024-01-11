9a73e82d995ee5c1d99b4c06ddee93b886ba9697
ArjunB126 <arjunbajare86@gmail.com>
2024-01-11 16:58:44 +0530

    Js_Changes

---
+++ script.js
@@ -0,0 +1,13 @@
+const inputSlider = document.querySelector("[data-lengthslider]");
+const lenghthDisplay = document.querySelector("[data-lengthNumber]");
+
+const passwordDisplay = document.querySelector("[data-passwordDisplay]");
+const copyBtn = document.querySelector("[data-copy]");
+const copyMsg = document.querySelector("[data-copyMsg]");
+const uppercaseCheck = document.querySelector("[#uppercase]");
+const lowerCaseCheck = document.querySelector("#lowercase");
+const numbersCheck = document.querySelector("#numbers");
+const symbolsCheck = document.querySelector("#symbols");
+const indicator = document.querySelector("[data-indicator]");
+const generateButton = document.querySelector(".generateBtn");
+const allCheckBox = document.querySelectorAll("input[type=checkbox]");
