6944f6b2396eed8534f4aef9290ea5404d1db524
ArjunB126 <arjunbajare86@gmail.com>
2024-01-11 17:06:02 +0530

    Js_Changes

--- script.js
+++ script.js
@@ -6,8 +6,14 @@ const copyBtn = document.querySelector("[data-copy]");
 const copyMsg = document.querySelector("[data-copyMsg]");
 const uppercaseCheck = document.querySelector("[#uppercase]");
 const lowerCaseCheck = document.querySelector("#lowercase");
-const numbersCheck = document.querySelector("#numbers");
+const numbersCheck = document.querySelector("#numbers"); 
 const symbolsCheck = document.querySelector("#symbols");
 const indicator = document.querySelector("[data-indicator]");
 const generateButton = document.querySelector(".generateBtn");
 const allCheckBox = document.querySelectorAll("input[type=checkbox]");
+
+let password = "";
+ let passwordLength = 10;
+ let checkCount = 1;
+ 
+  
